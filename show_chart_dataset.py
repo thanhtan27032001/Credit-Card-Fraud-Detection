927241fa1fcb7cd723c7d4c20ba6caa0e41b47ed
ThanhTan <thanhtan27032001@gmail.com>
2023-10-30 20:57:30 +0700

    [v1.0.0] Preprocessing - show bar chart

--- show_chart_dataset.py
+++ show_chart_dataset.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from matplotlib import pyplot as plt
 
 
-def show_dataset_ratio():
+def show_pie_chart():
     # raw_df = pd.read_csv('creditcard.csv')
 
     labels = ["Genuine", "Fraud"]
@@ -20,10 +20,12 @@ def show_dataset_ratio():
                  title="Fraud vs Genuine transactions")
     fig.show()
 
+
+def show_barchart():
     plt.figure(figsize=(3, 4))
     ax = sns.countplot(x='Class', data=raw_df)
     for i in ax.containers:
-        ax.bar_label(i, labels=raw_df['Class'].value_counts().tolist())
+        ax.bar_label(i, )
 
 
 # checking boxplots
@@ -39,11 +41,14 @@ def boxplots_custom(dataset, columns_list, rows, cols, suptitle):
 if __name__ == '__main__':
     raw_df = pd.read_csv('creditcard.csv')
 
-    # show_dataset_ratio()
+    show_pie_chart()
+    show_barchart()
 
     numeric_columns = (list(raw_df.loc[:, 'V1':'Amount']))
     boxplots_custom(dataset=raw_df, columns_list=numeric_columns, rows=8, cols=4, suptitle='Boxplots for each variable')
     plt.tight_layout()
     plt.show()
 
+    print('Genuine:', round(raw_df['Class'].value_counts()[0] / len(raw_df) * 100, 2), '% of the dataset')
+    print('Frauds:', round(raw_df['Class'].value_counts()[1] / len(raw_df) * 100, 2), '% of the dataset')
     print("All done")
